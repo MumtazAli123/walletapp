3480c610f010c41e0a693c1e34cdca6a47e73d8d
Ali Sindhi <64632604+MumtazAli123@users.noreply.github.com>
2023-01-28 05:30:06 +0500

    updatrd

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/alisindhi/StudioProjects/walletapp/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/alisindhi/StudioProjects/walletapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
